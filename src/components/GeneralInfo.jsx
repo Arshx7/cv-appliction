44448dafd5fe43c99cb0496070054b4aa2bc8593
Arshakh Haneen <arshx77@gmail.com>
2024-11-04 00:49:51 +0530

    feat: Add GeneralInfo component with labeled inputs for user details

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,28 +1,67 @@
-function GeneralInfo({generalInfo, setGeneralInfo}) {
-    
+function GeneralInfo({ generalInfo, setGeneralInfo }) {
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setGeneralInfo({
+      ...generalInfo,
+      [name]: value,
+    });
+  }
 
-    function handleChange(e) {
-
-        const name = e.target.name;
-        const value = e.target.value;
-        console.log(name)
-        console.log(value)
-        setGeneralInfo({
-            ...generalInfo,[name]:value
-        })   
-    }
-
-    return (
-        <div>
-            <h2>General Info</h2>
-            <form action="">
-                <input type = "text" placeholder="Name" name= "fullName" value={generalInfo.fullName} onChange={handleChange} /> 
-                <input type="email" placeholder="eample@123" name = "email" value={generalInfo.email} onChange={handleChange} />
-                <input type="number" placeholder="123456789" name="phoneNumber" value={generalInfo.phoneNumber} onChange={handleChange} />
-                <input type="text" placeholder="123djcjdcbdsbc" name="address" value={generalInfo.address} onChange={handleChange} />
-            </form>
+  return (
+    <div>
+      <h2>General Info</h2>
+      <form action="">
+        <div className="form-group">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            id="fullName"
+            placeholder="Name"
+            name="fullName"
+            value={generalInfo.fullName}
+            onChange={handleChange}
+          />
         </div>
-    )
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="example@123.com"
+            name="email"
+            value={generalInfo.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input
+            type="tel"
+            id="phoneNumber"
+            placeholder="123456789"
+            name="phoneNumber"
+            value={generalInfo.phoneNumber}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            placeholder="123 Main St"
+            name="address"
+            value={generalInfo.address}
+            onChange={handleChange}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default GeneralInfo;
